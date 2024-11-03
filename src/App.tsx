48e7f0f91ec2e6c84762d682cca1f46331064366
Joe Crachiolo <crachioloj@gmail.com>
2024-11-03 16:53:09 -0500

    Set up Vite and first page. (#1)
    Co-authored-by: Joe Crachiolo <jpc@Joes-MacBook-Pro.local>

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+      <h1>This page is under construction.</h1>
+      <h1>Check back soon!</h1>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div> */}
+      <p className="read-the-docs">
+        Joe Crachiolo
+      </p >
+      <a className="read-the-docs" href="mailto: crachioloj@gmail.com" > crachioloj@gmail.com </a>
+    </>
+  )
+}
+
+export default App
